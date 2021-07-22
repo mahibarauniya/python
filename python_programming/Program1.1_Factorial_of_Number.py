a47c05388a18aebd2f814009b0f85572fe63eace
Mahi Barauniya <mahimmm07@gmail.com>
2021-07-22 03:10:58 -0400

    first program for factorial ....

---
+++ python_programming/Program1.1_Factorial_of_Number.py
@@ -0,0 +1,24 @@
+import os
+
+def factorial(num):
+    result_num = ''
+    if num < 0:
+        result_num = 0
+    elif num == 0 or num == 1:
+        result_num = 1
+    else:
+        fact = 1
+        while (num > 1):
+            fact *= num
+            num -= 1
+        result_num = fact
+
+    return result_num
+
+def main():
+    num = 3
+    result = factorial(num)
+    print ("Number: {} and it's factorial is: {} ".format(num, result))
+
+if __name__ == "__main__":
+    main()
